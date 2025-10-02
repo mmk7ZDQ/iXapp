f5614c8951083fb65064797bb88fa2d06bbfce73
Your Wolf <zhuangjinrui0@gmail.com>
2025-10-02 20:36:20 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
-// Root build file - minimal placeholder
-plugins {
-    kotlin("multiplatform") version "1.9.10" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
